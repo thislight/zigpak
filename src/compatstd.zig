d4f3fead5abc2d1b7e797017823641570194d3a5
thislight <l1589002388@gmail.com>
2024-12-14 20:11:50 +0800

    compatstd: fix *FloatBig for 0.14

--- src/compatstd.zig
+++ src/compatstd.zig
@@ -4,6 +4,8 @@ const bytesAsValue = std.mem.bytesAsValue;
 const asBytes = std.mem.asBytes;
 const nativeEndian = @import("builtin").cpu.arch.endian();
 
+const isZig0d14AndLater = !builtin.zig_version.order(.{ .major = 0, .minor = 14, .patch = 0 }).compare(.lt);
+
 pub const mem = struct {
     pub fn writeIntBig(T: type, dst: *[@divExact(@typeInfo(T).Int.bits, 8)]u8, value: T) void {
         return std.mem.writeInt(T, dst, value, .big);
@@ -13,14 +15,20 @@ pub const mem = struct {
         return std.mem.readInt(T, src, .big);
     }
 
-    pub fn readFloatBig(comptime T: type, src: *const [@divExact(@typeInfo(T).Float.bits, 8)]u8) T {
-        const inf = @typeInfo(T).Float;
-        if (inf.bits > 64) {
+    fn bitsOfFloat(T: type) comptime_int {
+        if (isZig0d14AndLater) {
+            return @typeInfo(T).float.bits;
+        }
+        return @typeInfo(T).Float.bits;
+    }
+
+    pub fn readFloatBig(comptime T: type, src: *const [@divExact(bitsOfFloat(T), 8)]u8) T {
+        if (bitsOfFloat(T) > 64) {
             @compileError("readFloatBig does not support float types have more than 64 bits.");
         }
 
         if (nativeEndian == .little) {
-            var swapped: @typeInfo(@TypeOf(src)).Pointer.child = undefined;
+            var swapped: [@divExact(bitsOfFloat(T), 8)]u8 = undefined;
             for (0..@divExact(src.len, 2)) |i| {
                 const j = src.len - i - 1;
                 swapped[i] = src[j];
@@ -32,9 +40,8 @@ pub const mem = struct {
         return bytesAsValue(T, src).*;
     }
 
-    pub fn writeFloatBig(comptime T: type, dest: *[@divExact(@typeInfo(T).Float.bits, 8)]u8, val: T) void {
-        const inf = @typeInfo(T).Float;
-        if (inf.bits > 64) {
+    pub fn writeFloatBig(comptime T: type, dest: *[@divExact(bitsOfFloat(T), 8)]u8, val: T) void {
+        if (bitsOfFloat(T) > 64) {
             @compileError("writeFloatBig does not support float types have more than 64 bits.");
         }
         std.mem.copyForwards(u8, dest, asBytes(&val));
