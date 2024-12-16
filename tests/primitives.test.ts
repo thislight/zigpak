f32db1a91ef84418d0e073231dcc197342c98b3a
thislight <l1589002388@gmail.com>
2024-12-16 15:58:01 +0800

    tests: more tests
    * added tests about the int's and float's bound
    * unified tests about str and bin

--- tests/primitives.test.ts
+++ tests/primitives.test.ts
@@ -20,13 +20,48 @@ describe("bool", () => {
   test("false", primitive(false));
 });
 
+function maxIntOf(signed: boolean, bits: 8 | 16 | 24 | 32): number;
+function maxIntOf(signed: boolean, bits: number): number | bigint;
+
+function maxIntOf(signed: boolean, bits: number) {
+  const ibits = signed ? bits - 1 : bits;
+
+  if (ibits >= 52) {
+    return 2n ** BigInt(ibits) - 1n;
+  } else {
+    return 2 ** ibits - 1;
+  }
+}
+
+function minIntOf(signed: boolean, bits: number) {
+  if (!signed) {
+    return 0;
+  }
+
+  const ibits = signed ? bits - 1 : bits;
+
+  if (ibits >= 52) {
+    return -(2n ** BigInt(ibits));
+  } else {
+    return -(2 ** ibits);
+  }
+}
+
 describe("int", () => {
   test("0", primitive(0));
   test("1", primitive(1));
   test("-1", primitive(-1));
 
-  test("i64max", primitive(2n ** 63n - 1n));
-  test("i64min", primitive(-(2n ** 63n)));
+  for (const [signed, bits] of [8, 16, 32, 64].flatMap((x) => [
+    [true, x] as const,
+    [false, x] as const,
+  ])) {
+    test(`${signed ? "i" : "u"}${bits}max`, primitive(maxIntOf(signed, bits)));
+    if (signed) {
+      // No need to test 0 - they will be rewrited as fixint 0
+      test(`i${bits}min`, primitive(minIntOf(true, bits)));
+    }
+  }
 
   test("f64maxint", primitive(Number.MAX_SAFE_INTEGER));
   test("f64minint", primitive(Number.MIN_SAFE_INTEGER));
@@ -34,10 +69,20 @@ describe("int", () => {
 
 describe("float", () => {
   test("0.5", primitive(0.5));
-  test("1.5", primitive(1.0));
+  test("-0.5", primitive(-0.5));
+  test("f32maxint - 0.5", primitive(maxIntOf(true, 24) - 0.5));
+  test("f32minint + 0.5", primitive(maxIntOf(true, 24) + 0.5));
   test("f64max", primitive(Number.MAX_VALUE));
   test("f64min", primitive(Number.MIN_VALUE));
 });
+
+const TEST_NBYTES = [
+  0,
+  12,
+  maxIntOf(false, 8),
+  maxIntOf(false, 16),
+  maxIntOf(false, 16) * 2,
+]; // We could not test too large binary...
 
 function makeStr(bytes: number) {
   const strs = [] as string[];
@@ -47,20 +92,22 @@ function makeStr(bytes: number) {
   return strs.join();
 }
 
+const LARGEST_STR = makeStr(Math.max(...TEST_NBYTES));
+
 describe("str", () => {
-  test("0c", primitive(makeStr(0)));
-  test("12c", primitive("Hello World!"));
-  test("16384c", primitive(makeStr(16384)));
-  test("32698c", primitive(makeStr(16384)));
+  for (const nbytes of TEST_NBYTES) {
+    test(`${nbytes}c`, primitive(LARGEST_STR.slice(0, nbytes)));
+  }
 });
 
 function makeBin(bytes: number) {
   return crypto.getRandomValues(new Uint8Array(bytes));
 }
 
+const LARGEST_BIN = makeBin(Math.max(...TEST_NBYTES));
+
 describe("bin", () => {
-  test("0b", primitive(makeBin(0)));
-  test("12b", primitive(makeBin(12)));
-  test("16384b", primitive(makeBin(16384)));
-  test("32698b", primitive(makeBin(32698)));
+  for (const nbytes of TEST_NBYTES) {
+    test(`${nbytes}c`, primitive(LARGEST_BIN.slice(0, nbytes)));
+  }
 });
